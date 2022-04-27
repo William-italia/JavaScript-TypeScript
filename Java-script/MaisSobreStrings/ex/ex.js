2b41df128ada690d067d3878ee4435f6f1658d18
william-italia <williamitalia70@outlook.com>
2022-04-27 00:28:59 -0300

    1-1

--- Java-script/MaisSobreStrings/ex/ex.js
+++ Java-script/MaisSobreStrings/ex/ex.js
@@ -1,12 +1,12 @@
 
 const nome = prompt('Digite seu nome completo:');
 
-document.body.innerHTML += `Seu nome é: ${nome}<br />`;
-document.body.innerHTML += `Seu nome tem ${nome.length} letras <br />`;
-document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br />`; //charAt(1)
-document.body.innerHTML += `Qual o primeiro índice da letra i no seu nome? ${nome.indexOf('i')}<br />`;
-document.body.innerHTML += `Qual o último índice da letra i no seu nome? ${nome.lastIndexOf('i')}<br />`;
-document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3)}<br />`;
-document.body.innerHTML += `As palavras do seu nome são: ${nome.split(' ', 3)}<br />`;
-document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()}<br />`;
-document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()}<br />`;
+document.body.innerHTML += `Seu nome é: <strong>${nome}</strong> <br />`;
+document.body.innerHTML += `Seu nome tem <strong>${nome.length}</strong> letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: <strong>${nome[1]}</strong> <br />`; //charAt(1)
+document.body.innerHTML += `Qual o primeiro índice da letra i no seu nome? <strong>${nome.indexOf('i')}</strong> <br />`;
+document.body.innerHTML += `Qual o último índice da letra i no seu nome? <strong>${nome.lastIndexOf('i')}</strong> <br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <strong>${nome.slice(-3)}</strong> <br />`;
+document.body.innerHTML += `As palavras do seu nome são: <strong>${nome.split(' ', 3)}</strong> <br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <strong>${nome.toUpperCase()}</strong><br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <strong>${nome.toLowerCase()}</strong><br />`;
