b7d4227cf2cfb1e8bfc52d12df7a2c7016a32f49
william-italia <williamitalia70@outlook.com>
2022-04-27 13:36:41 -0300

    @-@

---
+++ Java-script/ObjetoMath/app.js
@@ -0,0 +1,27 @@
+let num1 = 9.44578;
+let num2 = 9;
+// Arredonda o numero pra baixo
+// let num2 = Math.floor(num1);
+
+// Arredonda o numero pra cima
+// let num2 = Math.ceil(num1);
+
+// Arredonda o numero
+// let num2 = Math.round(num1);
+
+// console.log(num2);
+
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+
+//Random pega um numero aleatorio entre 0 e 1
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
+console.log(aleatorio);
+
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
+
+
+console.log(num2 ** (1/2));
+console.log(num2 ** 0.5);
