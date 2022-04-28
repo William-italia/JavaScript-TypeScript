9c84556dd67a37281f3b0a78dd89d9432a44b8c6
william-italia <williamitalia70@outlook.com>
2022-04-28 15:45:56 -0300

    Bombeiro

---
+++ Java-script/js-logica/ifElse/app.js
@@ -0,0 +1,15 @@
+const hora = 24;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia! linda, casada?');
+}
+
+else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde! é aqui que chamaram o bombeiro?');
+}
+else if (hora >= 18 && hora <=23) {
+    console.log('vish vou dormir na rua hj');
+}
+else {
+    console.log('Desisto');
+}
