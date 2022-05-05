bebf0cd56ded2f485c52ca1841cc96e43ceb6d49
william-italia <williamitalia70@outlook.com>
2022-05-05 00:45:37 -0300

    Exercico feito

--- Java-script/js-logica/A_Modelo/assets/js/main.js
+++ Java-script/js-logica/A_Modelo/assets/js/main.js
@@ -9,22 +9,60 @@ const inputaltura = e.target.querySelector('#inputAltura');
 const peso = Number(inputpeso.value);
 const altura = Number(inputaltura.value);
 
-console.log(altura, peso);
 if (!peso) {
     setResultado('Peso Invalido!', false);
+    return;
+} 
+
+if (peso <= 45 || peso >= 500) {
+    setResultado('Digite um peso valido!', false);
     return;
 }
 
 if (!altura) {
     setResultado('Altura Invalida', false);
     return;
+} else if (altura <= 0.99 ) {
+    setResultado('Digite uma Altura valida!', false);
+    return;
 }
 
-if (!altura && !peso) {
- setResultado('Altura e peso invalidos', false);
- return;
-}
+const imc = getImc(peso, altura);
+const nivelImc = getNivelImc(imc);
+
+const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+setResultado(msg, true);
 });
+
+function getNivelImc (imc) {
+    const nivel = 
+    [
+    'Abaixo do peso',
+    'Peso Normal',  
+    'Sobrepeso',
+    'Obesidade grau 1', 
+    'Obesidade grau 2', 
+    'Obesidade grau 3'
+    ];
+
+    if (imc >= 39.9) { return nivel[5]; } 
+    
+    if (imc >= 34.9) { return nivel[4]; }
+    
+    if (imc >= 29.9) { return nivel[3]; } 
+    
+    if (imc >= 24.9) { return nivel[2]; } 
+    
+    if (imc >= 18.5) { return nivel[1]; } 
+    
+    if (imc < 18.5)  { return nivel[0]; }
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2); 
+}
 
 function criaP () {
     const p = document.createElement('p');
@@ -36,5 +74,13 @@ function setResultado (msg, isValid) {
     Resultado.innerHTML = '';
 
     const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+    p.innerHTML = msg;
     Resultado.appendChild(p);
 }
