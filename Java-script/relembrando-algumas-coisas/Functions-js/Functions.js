945c410b06eafe00e453c48e392f0bab196524c7
william-italia <williamitalia70@outlook.com>
2022-06-04 21:50:46 -0300

    x-x

---
+++ Java-script/relembrando-algumas-coisas/Functions-js/Functions.js
@@ -0,0 +1,24 @@
+// function soma(x = 1, y = 1) {
+//     const resultado = x + y;
+//     return `A soma dos valores de ${x} + ${y} é igual a ${resultado}`;
+// }
+
+// const resultado = soma(4, 2);
+// console.log(resultado);
+
+// const raiz = function (n) {
+//     return Math.sqrt(n);
+// };
+
+// console.log(raiz(9));
+
+// const pow = (x, y) => {
+//   return  Math.pow(x, y);
+// };
+
+// console.log(pow(5, 2));
+
+// Função reduzida
+const raiz = (n) => { return Math.sqrt(n) };
+
+console.log(raiz(9));
