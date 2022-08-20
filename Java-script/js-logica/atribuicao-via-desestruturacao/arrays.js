0acf2dbc1943d7c049c083303f84b67337fab44e
William-italia <williamitalia70@outlook.com>
2022-08-20 16:51:42 -0300

    x-x

---
+++ Java-script/js-logica/atribuicao-via-desestruturacao/arrays.js
@@ -0,0 +1,19 @@
+// let a = 'A';
+// let b = 'B';
+// let c = 'C';
+
+// const letras = [b, c, a];
+// [a, b, c] = letras;
+
+// console.log(a, b, c);
+//               0  1  2  3  4  5  6  7  8
+const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+// const primeiroNumero = numeros[0];
+
+
+const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros;
+console.log(primeiroNumero, segundoNumero, terceiroNumero);
+console.log(resto);
+
+
+
