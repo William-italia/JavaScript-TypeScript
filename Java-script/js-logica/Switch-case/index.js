9eb359caf19b73b14a8ba0dde104492154842918
william-italia <williamitalia70@outlook.com>
2023-02-28 21:07:31 -0300

    relembrando

--- Java-script/js-logica/Switch-case/index.js
+++ Java-script/js-logica/Switch-case/index.js
@@ -1,42 +1,80 @@
-function getDayText (diaSemana) {
+function getDayText (dayWeek) {
 
-    let diaSemanaTexto;
+    let dayWeekText;
+    const complement = "-Feira"
 
-
-    switch (diaSemana) {
+      switch (dayWeek) {
+        
         case 0:
-            diaSemanaTexto = 'Domingo';
-        return diaSemanaTexto;
+            dayWeekText = "Domingo";
+        return dayWeekText;
         case 1:
-            diaSemanaTexto = 'Segunda-Feira';
-        return diaSemanaTexto;
+            dayWeekText = (`Segunda${complement}`);
+        return dayWeekText;
         case 2:
-            diaSemanaTexto = 'Terça-Feira';
-        return diaSemanaTexto;
+            dayWeekText = (`Terça${complement}`);
+        return dayWeekText;
         case 3:
-            diaSemanaTexto = 'Quarta-Feira';
-        return diaSemanaTexto;
+            dayWeekText = (`Quarta${complement}`);
+        return dayWeekText;
         case 4:
-            diaSemanaTexto = 'Quinta-Feira';
-        return diaSemanaTexto;
+            dayWeekText = (`Quinta${complement}`);
+        return dayWeekText;
         case 5:
-            diaSemanaTexto = 'Sexta-Feira';
-        return diaSemanaTexto;
+            dayWeekText = (`Sexta${complement}`);
+        return dayWeekText;
         case 6:
-            diaSemanaTexto = 'Sábado';
-        return diaSemanaTexto;
-        default:
-            diaSemanaTexto = '';
-        return diaSemanaTexto;
+            dayWeekText = "Sabado";
+        return dayWeekText;
     }
- 
+    
+    if (dayWeek < 0 || dayWeek > 6) {
+        console.log(`Dia invalido, digite apenas numeros entre 0 e 6 sendo 0-Domingo e 6-Sabado\r`);
+        return "\r ";
+    }
 }
 
-const data = new Date('1987-04-20 00:00:00');
-const diaSemana = data.getDay();
-const diaSemanaTexto = getDayText(diaSemana);
+console.log(getDayText(4))
 
-console.log(diaSemana, diaSemanaTexto);
+// function getDayText (diaSemana) {
+
+//     let diaSemanaTexto;
+
+
+//     switch (diaSemana) {
+//         case 0:
+//             diaSemanaTexto = 'Domingo';
+//         return diaSemanaTexto;
+//         case 1:
+//             diaSemanaTexto = 'Segunda-Feira';
+//         return diaSemanaTexto;
+//         case 2:
+//             diaSemanaTexto = 'Terça-Feira';
+//         return diaSemanaTexto;
+//         case 3:
+//             diaSemanaTexto = 'Quarta-Feira';
+//         return diaSemanaTexto;
+//         case 4:
+//             diaSemanaTexto = 'Quinta-Feira';
+//         return diaSemanaTexto;
+//         case 5:
+//             diaSemanaTexto = 'Sexta-Feira';
+//         return diaSemanaTexto;
+//         case 6:
+//             diaSemanaTexto = 'Sábado';
+//         return diaSemanaTexto;
+//         default:
+//             diaSemanaTexto = '';
+//         return diaSemanaTexto;
+//     }
+ 
+// }
+
+// const data = new Date('1987-04-20 00:00:00');
+// const diaSemana = data.getDay();
+// const diaSemanaTexto = getDayText(diaSemana);
+
+// console.log(diaSemana, diaSemanaTexto);
 
 
 
---
+++ Java-script/js-logica/Switch-case/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+f (dayWeek < 0 || dayWeek > 6) {
+            return 
+            console.log(`Dia invalido, digite apenas numeros entre 0 e 6 sendo 0-Domingo e 6-Sabado`);
+        }
