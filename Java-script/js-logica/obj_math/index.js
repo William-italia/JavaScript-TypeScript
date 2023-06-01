6e170e78c1d8912973bbded0e1de1c8b6b3801a8
william-italia <williamitalia70@outlook.com>
2023-06-01 20:03:00 -0300

    subindo

---
+++ Java-script/js-logica/obj_math/index.js
@@ -0,0 +1,22 @@
+
+
+
+const alunos = ['Luiz', 'João', 'Maria'];
+
+// delete alunos[2];
+console.log(alunos);
+
+// const removido = alunos.shift(); remove o primeiro da lista
+// const removido = alunos.pop(); remove o ultimo da lista
+
+// alunos.unshift('Luiza') unshift add no inicio do array
+// alunos.unshift('Fabio ')
+
+// alunos.push('Rodrigo'); add no fim do array
+// alunos.push('Luiza');
+
+// alunos[alunos.length] = 'Luiza'
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Luiza';
+// console.log(alunos);
+// console.log(alunos[0]);
