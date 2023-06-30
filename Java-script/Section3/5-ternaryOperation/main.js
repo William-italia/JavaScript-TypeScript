bdc47a3da49ba814902f5e9571a624226d37f66f
william-italia <williamitalia70@outlook.com>
2023-06-30 14:49:41 -0300

    table imc, ternaryOperation, ObjectDate

---
+++ Java-script/Section3/5-ternaryOperation/main.js
@@ -0,0 +1,16 @@
+// ? :
+const pontuacaoUsuario = 1900;
+const nvlUser = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Comum';
+
+const corUserOp = 'Roxo';
+const corUserbg = corUserOp || 'Black';
+
+
+console.log(nvlUser, corUserbg);
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuário Comum');
+// }
+
---
+++ Java-script/Section3/z-exercises/tabelaImc/assets/js/main.js
@@ -0,0 +1,66 @@
+// capturar evento de submit do formulario
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+
+    if (!peso) {
+        setResultado("Peso Inválido", false);
+        return;
+    }
+
+    if (!altura) {
+        setResultado("Altura Inválida", false);
+        return;
+    }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu Imc é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+});
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2
+    return imc.toFixed(2);
+} 
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso Normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if  (imc < 18.5) return nivel[0];    
+}
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+}
+
+function setResultado (msg, isValid) { 
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('success');
+    } else {
+        p.classList.add('error');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
