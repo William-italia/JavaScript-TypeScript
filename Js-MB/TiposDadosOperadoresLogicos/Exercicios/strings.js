c8d3369cda4bf0c708b0ce08c02d32862e98041e
william-italia <williamitalia70@outlook.com>
2023-11-17 10:40:05 -0300

    for loop

--- Js-MB/TiposDadosOperadoresLogicos/Exercicios/strings.js
+++ Js-MB/TiposDadosOperadoresLogicos/Exercicios/strings.js
@@ -1,3 +1,11 @@
-console.log('Bom dia');
-console.log("Bom dia");
-console.log(`Bom dia`);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+const semPares = []
+
+for (let i of arr) {
+  if (i % 2 == 0) {
+
+  } else {
+    semPares.push(i);
+  }
+  console.log(semPares);
+}
