ee091bc84cf708152db0b5c4d2a9af02b9001a95
Chart 70 <williamitalia70@outlook.com>
2024-01-25 15:58:18 -0300

    calculadora IMC

---
+++ Java-script/logica/ExCalcImc/assets/js/app.js
@@ -0,0 +1,76 @@
+function calculadoraImc() {
+
+    const form = document.querySelector("#form");
+
+    form.addEventListener('submit', function (e) {
+
+        e.preventDefault();
+
+        const inputPeso = e.target.querySelector("#peso");
+        const inputAltura = e.target.querySelector("#altura");
+
+        const peso = Number(inputPeso.value);
+        const altura = Number(inputAltura.value);
+
+        if (!peso && !altura) {
+            setResultado("Digite valores validos", false);
+            return;
+        }
+        if(!peso) {
+            setResultado("Digite um peso valido", false);
+            return;
+        }
+        if(!altura) {
+            setResultado("Digite uma altura valida", false);
+            return;
+        }
+
+        const imc = getIMC(altura, peso);   
+        const nivel = getNivelImc(imc);
+
+        const msg = `Seu imc é: ${imc}, ${nivel}`;
+
+        setResultado(msg, true);
+    });
+
+    function getIMC(altura, peso) {
+        const imc = peso / altura ** 2;
+        return imc.toFixed(2);
+    };
+
+    function getNivelImc(imc) {
+        const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+        'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+        if (imc >= 39.9) return nivel[5];
+        if (imc >= 34.5) return nivel[4];
+        if (imc >= 29.9) return nivel[3];
+        if (imc >= 24.9) return nivel[2];
+        if (imc >= 18.5) return nivel[1];
+        if (imc < 18.5) return nivel[0];
+    };
+
+    function criaP() {
+        const p = document.createElement('p');
+        p.classList.add('result');
+        return p;
+    }
+
+    function setResultado(msg, isValid) {
+        const resultado = document.querySelector('.resultado');
+        resultado.innerHTML = '';
+
+        const p = criaP();
+
+        if(isValid){
+            p.classList.add('success');
+        } else {
+            p.classList.add('bad');
+        }
+
+        p.innerHTML = msg;
+        resultado.appendChild(p);
+    }
+}
+
+calculadoraImc();
