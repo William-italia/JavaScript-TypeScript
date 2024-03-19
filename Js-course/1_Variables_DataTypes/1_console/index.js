67f6c3308875e2af89e08c4a0a2f9701e4303191
Chart 70 <williamitalia70@outlook.com>
2024-03-19 19:01:38 -0300

    up

---
+++ Js-course/1_Variables_DataTypes/1_console/index.js
@@ -0,0 +1,20 @@
+console.log(100)
+console.log("Olá mundo!")
+
+console.log(20 ,'hello', true)
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+console.warn('Warning');
+
+console.table({name: 'william', email: 'williamitalia@outlook.com'});
+
+console.group('simple')
+console.error('Alert');
+console.warn('Warning');
+
+console.table({name: 'william', email: 'williamitalia@outlook.com'});
+console.groupEnd();
