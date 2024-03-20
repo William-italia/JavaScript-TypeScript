56d141a5bbad6913401736d01b560e4a417654ba
Chart 70 <williamitalia70@outlook.com>
2024-03-20 17:07:26 -0300

    up

---
+++ Js-course/1_Variables_DataTypes/5_operators/index.js
@@ -0,0 +1,39 @@
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+x = 5 ** 5;
+
+// concatenation
+x = 'hello' + ' ' + 'World!';
+
+// exponent
+x = 2 ** 3;
+
+// increment
+x = 2;
+x = x + 1;
+x += 1;
+x++;
+ 
+// decrement
+x--;
+
+// Assignment operators
+x = 10;
+x += 5;
+
+// comparison operators
+x = 2 == 2;
+x = 2 != 4;
+x = 2 === '2';
+x = 2 !== '2';
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
---
+++ Js-course/1_Variables_DataTypes/6_type_coercion/index.js
@@ -0,0 +1,8 @@
+let x;
+
+x = 5 + '5';
+x = 5 + +'5'; //Number('5')
+
+
+
+console.log(x, typeof x);
