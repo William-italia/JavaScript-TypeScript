c6fe6e571d8af34d9a477c53b7c6b43b8f34f4ee
William Italia <williamitalia70@outlook.com>
2024-08-07 18:58:01 -0300

    up

---
+++ Aulas-js/Variaveis e tipos de dados/variaveis_&_constantes/app.js
@@ -0,0 +1,15 @@
+// let
+//  0  1  2  3  4
+const numeros = [1, 2, 3, 4, 5];
+
+const pessoa = {
+    name: 'jorge'
+};
+
+numeros.push(6, 7, 8, 9, 10);
+pessoa.name = 'william';
+pessoa.email = 'william@gmail.com';
+
+
+console.log(pessoa);
+
