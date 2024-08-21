8afc061680b19b13b381deabf2d3e7c62780848a
William Italia <williamitalia70@outlook.com>
2024-08-21 17:12:25 -0300

    up

--- Js-course/09-asynchronous-javascript/5-kokeGeneratorProjectChallenge/script.js
+++ Js-course/09-asynchronous-javascript/5-kokeGeneratorProjectChallenge/script.js
@@ -1,7 +1,28 @@
-const xhr = new XMLHttpRequest();
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
 
-xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
 
-xhr.onreadystatechange = function () {
-    console.log(this.responseText);
+const generateJoke = () => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+                jokeEl.innerHTML = JSON.parse(this.responseText).value;
+            } else {
+                jokeEl.innerHTML = 'Algo deu errado! (Não é engraçado)';
+            }
+        }
+    }
+
+    xhr.send();
 }
+
+
+
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
+
+
